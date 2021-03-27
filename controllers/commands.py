40a44063ee86c82404b4d0b715936dfd90d93b5a
Unknown <Ty@gimpel.ca>
2021-03-26 19:42:40 -0700

    Added display to invalid command message. Now tells user valid commands.

--- controllers/commands.py
+++ controllers/commands.py
@@ -5,6 +5,11 @@ class CommandsController():
             "test": self.testFunc,
             "repeat": self.repeatFunc
         }
+
+    @property
+    def valid_commands(self):
+        return self._cmdList.keys()
+    
     async def testFunc(self, ctx, args):
         await ctx.channel.send(f"This is the test command.\nArgs:{args}")
 
@@ -13,11 +18,16 @@ class CommandsController():
         if len(args) > 0:
             msg = msg + (' '.join(args).capitalize())
         else:
-            msg = msg.join("N/A")
+            msg = msg + 'N/A'
         await ctx.channel.send(msg)
 
     async def invalidCommand(self, ctx, args):
-        await ctx.channel.send("Invalid command.")
+        msg = f"Invalid command. Valid commands are:\n"
+        print(self.valid_commands)
+        for cmd in self.valid_commands:
+            print(cmd)
+            msg = msg + f"\t\t!{cmd}\n"
+        await ctx.channel.send(msg)
 
     async def exec(self, ctx, command, args):
         await self._cmdList.get(command, self.invalidCommand)(ctx, args)
